67a71220fc7425e7268f9d26f2e644fec5e0879e
asyncguo <asyncguo@163.com>
2022-06-25 22:45:12 +0800

    feat(types): add types module

---
+++ packages/types/src/index.ts
@@ -0,0 +1,52 @@
+/**
+ * 不同 ast 需要遍历的属性集合
+ */
+const astDefinationsMap: Map<string, {
+  visitor?: string[]
+  isBlock?: boolean
+}>  = new Map()
+
+astDefinationsMap.set('Program', {
+  visitor: ['body'],
+  isBlock: true
+});
+astDefinationsMap.set('VariableDeclaration', {
+  visitor: ['declarations']
+});
+astDefinationsMap.set('VariableDeclarator', {
+  visitor: ['id', 'init']
+});
+astDefinationsMap.set('Identifier', {});
+astDefinationsMap.set('NumericLiteral', {});
+astDefinationsMap.set('FunctionDeclaration', {
+  visitor: ['id', 'params', 'body'],
+  isBlock: true
+});
+astDefinationsMap.set('BlockStatement', {
+  visitor: ['body']
+});
+astDefinationsMap.set('ReturnStatement', {
+  visitor: ['argument']
+});
+astDefinationsMap.set('BinaryExpression', {
+  visitor: ['left', 'right']
+});
+astDefinationsMap.set('ExpressionStatement', {
+  visitor: ['expression']
+});
+astDefinationsMap.set('CallExpression', {
+  visitor: ['callee', 'arguments']
+});
+
+const validations: Record<string, (node) => boolean> = {}
+
+for (const name of astDefinationsMap.keys()) {
+  validations['is' + name] = function (node) {
+    return node.type === name
+  }
+}
+
+export {
+  astDefinationsMap as visitorKeys,
+  validations
+}
